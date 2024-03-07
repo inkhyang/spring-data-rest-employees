cb8ef7fee1c89f3682aa4bd9f93e47e2b10a1d42
inkhyang <adeqvatn@gmail.com>
2024-03-08 01:03:38 +0400

    init app

---
+++ src/test/java/com/inkhyang/employees/ApplicationTest.java
@@ -0,0 +1,4 @@
+package com.inkhyang.employees;
+
+class ApplicationTest {
+}
---
+++ src/test/java/com/inkhyang/employees/repository/EmployeeRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.inkhyang.employees.repository;
+
+import com.inkhyang.employees.domain.Employee;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class EmployeeRepositoryTest {
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @Test
+    void should_save_employee(){
+        //given
+        var employee = new Employee();
+        employee.setName("Vasya");
+        employee.setRole("Alcoholic");
+        //when
+        employeeRepository.save(employee);
+        //then
+        var result = employeeRepository.findAll();
+        assertThat(result)
+                .isNotEmpty()
+                .hasSize(1);
+    }
+}
